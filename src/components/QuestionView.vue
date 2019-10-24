4a900a4b01138efb5d7cdc8e5ce8cd2dbb1a42af
Andre <asalmeri@stud.hs-bremen.de>
2019-10-24 14:29:22 +0200

    if you sort the array, maybe use it too

--- src/components/QuestionView.vue
+++ src/components/QuestionView.vue
@@ -51,6 +51,7 @@ export default {
       var _response =  await this.$api.db.question.get({}, this.$tkn);
       var q = _response.data.reverse();
       var qf = q.filter(function(value){
+        console.log(value.userID,that.$api.usr.id);
         return value.userID != that.$api.usr.id;
       })
       for(var i = 0; i < q.length; i++){
